3c4c2a2744fa4c08681f19ee26af5b139f59549b
MrLegend-Robby <35876269+MrLegend-Robby@users.noreply.github.com>
2018-08-21 14:10:39 +0300

    spacedreams1

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const colors = ['92ce14', '4bd588', '4bd5b9', 'd54b76', '00edff', '0077ff', 'f2ff00', 'cbd073', 'd09573', 'f2070b',];
+const colors = ['92ce14', '4bd588', '4bd5b9', 'd54b76', '00edff', '0077ff', 'f2ff00', 'cbd073', 'd09573', 'f2070b'];
 function color () {
   colors.forEach(function (item, number) {
-      setTimeout(function () {bott.guilds.get('481416300414107648').roles.get('481047766093791237').setColor(item).catch();if(number === colors.length-1) setTimeout(function () {color()}, 1000)}, number*1000);
+      setTimeout(function () {bott.guilds.get('481047766093791237').roles.get('481416300414107648').setColor(item).catch();if(number === colors.length-1) setTimeout(function () {color()}, 1000)}, number*1000);
   });
 }
 
